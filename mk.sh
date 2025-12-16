b0ecd85b83cdd45e2a3a33938fd8dc1ba83fb49c
sweetsoop <sweetsoop@gmail.com>
2025-12-16 13:09:34 +0900

    Update .gitignore to include additional level files

---
+++ mk.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+python3 NtS1LevelConvert.py levels
+ca65 src/nova.s -o src/nova.o -l nova.lst -g
+ld65 -C src/nova.x src/nova.o -o nova.nes -m map.txt --dbgfile debug.dbg
+
+
+
+
---
+++ tools/convertdialog.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+./mkdialog dictionary.txt dialog.txt dialog.s
+
+
+
+
